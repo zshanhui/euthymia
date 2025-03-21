4837cc314192bb88d66c856f06c5fdfbc9771ff0
zshanhui <shanhui.dev@proton.me>
2025-03-21 18:34:12 +0800

    edit landing page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { use, useState, Suspense } from 'react';
 import { Button } from '@/components/ui/button';
-import { CircleIcon, Home, LogOut } from 'lucide-react';
+import { BookOpenText, Home, LogOut } from 'lucide-react';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -84,8 +84,8 @@ function Header() {
     <header className="border-b border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center">
         <Link href="/" className="flex items-center">
-          <CircleIcon className="h-6 w-6 text-orange-500" />
-          <span className="ml-2 text-xl font-semibold text-gray-900">ACME</span>
+          <BookOpenText className="h-6 w-6 text-fuchsia-500" />
+          <span className="ml-2 text-xl font-semibold text-gray-900">Euthymia</span>
         </Link>
         <div className="flex items-center space-x-4">
           <Suspense fallback={<div className="h-9" />}>
